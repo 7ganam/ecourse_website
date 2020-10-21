9212e835dc74bbe8541176133953258156921c27
7ganam <7ganam@gmail.com>
2020-10-21 07:43:30 +0200

    add card automatically when user increase the number of sessions

--- src/components/pages/NewCourseComponent/NewCourseComponent.js
+++ src/components/pages/NewCourseComponent/NewCourseComponent.js
@@ -27,9 +27,7 @@ class NewCourse extends Component {
             CourseDescription: '',
             startDate: new Date(),
             endDate: new Date(),
-            Session_1_title: '',
-            Session_1_Description: '',
-            number_of_sessions: '',
+            number_of_sessions: 1,
             // Session_1_startDate: new Date(),
             Sessions: [{
                 Session_title: '',
@@ -37,12 +35,7 @@ class NewCourse extends Component {
                 Session_startDate: '',
                 isOpen: false,
             },
-            {
-                Session_title: '',
-                Session_Description: '',
-                Session_startDate: '',
-                isOpen: false,
-            }
+
             ]
         };
 
@@ -52,14 +45,16 @@ class NewCourse extends Component {
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
-        this.handleStartChange = this.handleStartChange.bind(this);
-        this.handleEndChange = this.handleEndChange.bind(this);
         this.handleEndChange = this.session_date_change_handler_function_factory.bind(this);
         this.handle_session_InputChange_factory = this.handle_session_InputChange_factory.bind(this);
+        this.handle_number_of_sessions_Change = this.handle_number_of_sessions_Change.bind(this);
+
 
 
     }
+
+
+
 
     handleInputChange(event) {
         const target = event.target;
@@ -71,6 +66,61 @@ class NewCourse extends Component {
         });
     }
 
+
+    handle_number_of_sessions_Change(event) {
+        //change the number
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+
+        // if the number increase add empty session objects to the State.Sessions
+        // if the number decreased slice the State.Sessions to the new number of state 
+
+
+        let new_number_of_sessions = target.type === 'checkbox' ? target.checked : target.value;
+        let old_number_of_sessions = this.state.number_of_sessions;
+
+
+        if (new_number_of_sessions > old_number_of_sessions) {
+            let difference_in_session_numbers = new_number_of_sessions - old_number_of_sessions;
+            let new_empty_Sessions = [];
+            let i;
+            for (i = 0; i < difference_in_session_numbers; i++) {
+                new_empty_Sessions.push(
+                    {
+                        Session_title: '',
+                        Session_Description: '',
+                        Session_startDate: '',
+                        isOpen: false,
+                    }
+                )
+            }
+            // 1. Make a shallow copy of the Sessions
+            let Sessions = [...this.state.Sessions];
+            let New_Sessions = Sessions.concat(new_empty_Sessions);
+            this.setState({
+                Sessions: New_Sessions
+            });
+        }
+        else if (new_number_of_sessions < old_number_of_sessions) {
+            let Sessions = [...this.state.Sessions];
+            let New_Sessions = Sessions.slice(0, new_number_of_sessions);
+            this.setState({
+                Sessions: New_Sessions
+            });
+        }
+
+
+
+
+    }
+
+
+
     handleSubmit(event) {
         console.log('Current State is: ' + JSON.stringify(this.state));
         alert('Current State is: ' + JSON.stringify(this.state));
@@ -80,23 +130,12 @@ class NewCourse extends Component {
     }
 
 
-    handleStartChange(date) {
-        this.setState({
-            startDate: date
-        })
-    }
 
-    handleEndChange(date) {
-        this.setState({
-            endDate: date
-        })
-    }
 
     session_date_change_handler_function_factory(in_session_index) {
         const session__index = in_session_index; // closure variable different for every instance of the returned functions
         return (
             (date) => {
-
                 // 1. Make a shallow copy of the Sessions
                 let Sessions = [...this.state.Sessions];
                 // 2. Make a shallow copy of the Session you want to mutate
@@ -177,21 +216,31 @@ class NewCourse extends Component {
 
             <Card >
                 <CardHeader style={this.card_head_style} className="session_card_head">
-                    <span>Session {index + 1} </span>
-                    <Button color="primary" onClick={this.handle_session_card_toggle_factory(index)} style={{ marginBottom: '1rem' }}>Toggle</Button>
-                </CardHeader>
-                <Collapse isOpen={this.state.Sessions[index].isOpen}>
-                    <CardBody>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <span>Session {index + 1} </span>
 
-
-                        <FormGroup row>
-                            <Label for={"Session__title"} sm={3}><span className="Session_label">Session title:</span></Label>
-                            <Col sm={9} className="ml-auto">
+                            </Col>
+                            <Col>
                                 <Input type="text" name={"Session_title"} id={"Session_" + index + "_title"} placeholder="enter session title here"
                                     value={this.state.Sessions[index].Session_title}
                                     onChange={this.handle_session_InputChange_factory(index)} />
                             </Col>
-                        </FormGroup>
+                            <Col>
+                                <Button color="primary" onClick={this.handle_session_card_toggle_factory(index)} style={{ marginBottom: '1rem' }}>Toggle</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+
+
+
+
+                </CardHeader>
+
+                <Collapse isOpen={this.state.Sessions[index].isOpen}>
+                    <CardBody>
+
 
                         <FormGroup row>
                             <Label for="Session_Description" sm={3}> <span className="Session_label">Session Description:</span></Label>
@@ -201,7 +250,6 @@ class NewCourse extends Component {
                                     onChange={this.handle_session_InputChange_factory(index)} />
                             </Col>
                         </FormGroup>
-
 
                         <FormGroup row>
                             <Label for="session_date" sm={3}> <span className="Session_label">Session start time:</span></Label>
@@ -271,7 +319,7 @@ class NewCourse extends Component {
                                                 <Label for="number_of_sessions" sm={3}> <span className="new_course_label">number of sessions:</span></Label>
                                                 <Col sm={9}>
                                                     <Input id="number_of_sessions" type="number" min="1" max="30" name="number_of_sessions" value={this.state.number_of_sessions}
-                                                        onChange={this.handleInputChange} />
+                                                        onChange={this.handle_number_of_sessions_Change} />
                                                 </Col>
                                             </FormGroup>
 
